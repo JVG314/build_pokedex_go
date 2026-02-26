70df909e417a98efc0dcaff61ff58913b21f42a3
jvidal <joan.vidal.ginard@gmail.com>
2026-02-26 17:07:53 +0100

    Lesson4

--- repl.go
+++ repl.go
@@ -7,14 +7,55 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+// defined here so we can use it later inside commandHelp to iterate over it
+// but initialized inside startRepl() to avoid initialization circular dependency
+// as commands is being initialized as  package load time, commandHelp is referenc
+// ed in that initialization but commandHelp tries to use commands to iterate over it
+// so we define here but initalize later
+var commands map[string]cliCommand
+
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
 	words := strings.Fields(lowered)
 	return words
 }
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	for _, command := range commands {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
+}
+
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
+	commands = map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 	for {
 		fmt.Print("Pokedex > ")
 		if !scanner.Scan() {
@@ -25,6 +66,16 @@ func startRepl() {
 		if len(cleaned) == 0 {
 			continue
 		}
-		fmt.Printf("Your command was: %s\n", cleaned[0])
+		// fmt.Printf("Your command was: %s\n", cleaned[0])
+		command, exists := commands[cleaned[0]]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+
 	}
 }
