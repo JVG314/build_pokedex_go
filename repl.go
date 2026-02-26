d78a12f35f9c4c4bd437d7e6f6547a04ec7c2a96
jvidal <joan.vidal.ginard@gmail.com>
2026-02-26 14:39:54 +0100

    Lesson3

--- repl.go
+++ repl.go
@@ -1,9 +1,30 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
 	words := strings.Fields(lowered)
 	return words
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			break
+		}
+		input := scanner.Text()
+		cleaned := cleanInput(input)
+		if len(cleaned) == 0 {
+			continue
+		}
+		fmt.Printf("Your command was: %s\n", cleaned[0])
+	}
 }
